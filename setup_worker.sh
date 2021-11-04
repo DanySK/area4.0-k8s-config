8543d88fcc04a2f15d3281f7a97c1cd7bf426457
Danilo Pianini <danilo.pianini@unibo.it>
2021-11-04 18:54:47 +0100

    Update setup_worker.sh

--- setup_worker.sh
+++ setup_worker.sh
@@ -10,3 +10,16 @@ sudo systemctl stop containerd
 sudo swupd bundle-add git
 sudo mkdir -p /etc/containers/
 sudo curl -sL -o /etc/containers/policy.json $GH_RAW/$REPO/$REF/etc/containers/policy.json
+
+sudo mkdir -p /etc/sysctl.d/
+sudo tee /etc/sysctl.d/99-kubernetes-cri.conf > /dev/null <<EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system
+
+sudo systemctl mask $(sed -n -e 's#^/dev/\([0-9a-z]*\).*#dev-\1.swap#p' /proc/swaps) 2>/dev/null
+sudo swapoff -a
+
+
